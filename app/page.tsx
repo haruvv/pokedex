e3da879044feef2a110a57a21a735c6423598d7f
haruvv <haruki.ito0044@gmail.com>
2024-07-27 06:40:59 +0900

    エラー修正

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,14 @@ import { Links } from '@/components/Links';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Haedline title="index Page" />
+      <Haedline
+        title="index Page"
+        number={11111}
+        array={['1', '2', 'S']}
+        obj={{ foo: 'foo', key: 'key', id: 1 }}
+        boo={true}
+        cmp={<p>hoo</p>}
+      />
       <Links />
     </main>
   );
